84944716bb593d45773b505b24453ba92d21152c
gregorylira <gregory.lira007@gmail.com>
2024-06-02 10:05:16 -0300

    first use case

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,20 @@
+import { Answer } from "../entities/answer";
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+interface AnswerQuestionUseCaseResponse {
+  success: boolean;
+  message?: string;
+}
+
+export class AnswerQuestionUseCase {
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer(content);
+
+    return answer;
+  }
+}
