939cc20d3411cb2cf6f166414116f95dd96a5234
gregorylira <gregory.lira007@gmail.com>
2024-06-11 10:46:23 -0300

    map propeties

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,13 @@
 import { randomUUID } from "crypto";
 import { Entitiy } from "../../core/entities/entities";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface AnswerProps {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
   content: string;
-  authorId: string;
-  questionId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entitiy<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,16 @@
 import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
 import { Entitiy } from "../../core/entities/entities";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entitiy<QuestionProps> {
